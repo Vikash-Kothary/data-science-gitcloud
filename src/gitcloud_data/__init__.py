80717a1ff101f9a70b8237053429f3134dcb95fc
Vikash Kothary <kothary.vikash@gmail.com>
2021-12-22 23:12:59 +0000

    feat: Separate integration endpoints and data endpoints

--- src/gitcloud_data/__init__.py
+++ src/gitcloud_data/__init__.py
@@ -3,12 +3,22 @@ from fastapi import APIRouter
 data = APIRouter()
 
 @data.get("/local")
+async def get_local_integration_details():
+    local = {}
+    return {"data": local}
+
+@data.post("/local")
+async def create_local_integration():
+    local = {}
+    return {"data": local}
+
+@data.get("/local/data")
 async def get_local_data():
     local = {}
     return {"data": local}
 
 @data.get("/github")
-async def get_github_data():
+async def get_github_integration_details():
     github = {}
     return {"data": github}
 
@@ -17,12 +27,22 @@ async def create_github_integration():
     github = {}
     return {"data": github}
 
+@data.get("/github/data")
+async def get_github_data():
+    github = {}
+    return {"data": github}
+
 @data.get("/gitlab")
-async def get_gitlab_data():
+async def get_gitlab_integration_details():
     gitlab = {}
     return {"data": gitlab}
 
 @data.post("/gitlab")
 async def create_gitlab_integration():
+    gitlab = {}
+    return {"data": gitlab}
+
+@data.get("/gitlab/data")
+async def get_gitlab_data():
     gitlab = {}
     return {"data": gitlab}
