da1e7b605496d7151c4e3fd40100a2789c67df91
Vikash Kothary <kothary.vikash@gmail.com>
2021-12-22 18:34:32 +0000

    feat: Add more data points to repo example

--- src/gitcloud_features/__init__.py
+++ src/gitcloud_features/__init__.py
@@ -38,7 +38,22 @@ async def get_repositorie_by_id(repo_id: int):
     repo['organisation'] = 'vikash-kothary'
     repo['group'] = 'data-sciece-projects'
     repo['name'] = 'data-science-gitcloud'
+    repo['description'] = ''
+    repo['license'] = 'AGPLv3'
     repo['private'] = True
+    repo['remote_origin'] = 'https://gitlab.com/vikash-kothary/data-sciece-projects/data-science-gitcloud'
+    repo['remote_upstream'] = 'htpps://github.com/vikash-kothary/data-science-gitcloud'
+    repo['branch_main'] = 'main'
+    repo['branch_main_remote'] = 'upstream'
+    repo['branch_develop'] = 'develop'
+    repo['branch_develop_remote'] = 'origin'
+    repo['is_local'] = True
+    repo['has_git_folder'] = True
+    repo['has_untracked_files'] = True
+    repo['has_uncommitted_changes'] = True
+    repo['has_unpushed_changes'] = True
+    repo['has_unpulled_changes'] = False
+    repo['commits_count'] = 1
     return {"data": repo}
 
 @features.get("/templates")
